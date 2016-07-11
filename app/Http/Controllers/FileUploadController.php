8d9f2c583ba59ee2b374267f698cd2775eb5dd08
arnondora <peter_arnon1@hotmail.com>
2016-07-12 00:49:37 +0700

    Add Upload function

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,10 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Illuminate\Support\Facades\Input;
+
+use Illuminate\Support\Facades\File;
+
+use Storage;
+
 class FileUploadController extends Controller
 {
     public function home ()
     {
       return view ('home');
+    }
+
+    public function upload ()
+    {
+      //upload file
+      $fileName = Input::file('fileUpload')->getClientOriginalName();
+      Storage::disk('public')->put($fileName, Input::file('fileUpload'));
+
+      // print_r($fileName);
+      return back();
     }
 }
